0a1294b110b343bb92d5ee9429cb4303b53bf1a7
Marco Sabatini <marco.sabatini@external-rumbo.com>
2016-03-04 15:23:43 +0100

    removed angular dips

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,34 @@
-module.exports = function(config){
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    basePath : './',
+        basePath: './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/underscore/underscore.js',
-        'app/bower_components/jquery/dist/jquery.js',
-        'app/bower_components/backbone/backbone.js',
-      'app/components/**/*.js',
-      'app/lib/**/*.js',
-      'app/view*/**/*.js'
-    ],
+        files: [
+            'app/bower_components/underscore/underscore.js',
+            'app/bower_components/jquery/dist/jquery.js',
+            'app/bower_components/backbone/backbone.js',
+            'app/lib/**/*.js',
+            'app/view*/**/*.js'
+        ],
 
-    autoWatch : true,
+        autoWatch: true,
 
-    frameworks: ['jasmine-ajax','jasmine'],
+        frameworks: ['jasmine-ajax', 'jasmine'],
 
-    browsers : ['Chrome'],
+        browsers: ['Chrome'],
 
-    plugins : [
+        plugins: [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
             'karma-jasmine-ajax',
             'karma-junit-reporter'
-            ],
+        ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
 
-  });
+    });
 };
