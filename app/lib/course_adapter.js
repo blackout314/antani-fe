1b276a211771ebaad70296ab7dc4194ac76337f3
Fabrizio Machella <fmachella@lastminute.com>
2016-03-04 16:17:13 +0100

    adapter configurato

--- app/lib/course_adapter.js
+++ app/lib/course_adapter.js
@@ -1,17 +1,30 @@
+'use strict'
+
 var RestAdapter = function (serviceEndpointBaseURL) {
- this.serviceEndpointBaseURL = serviceEndpointBaseURL || "https://antani-be.herokuapp.com";
+    this.serviceEndpointBaseURL = serviceEndpointBaseURL || "https://antani-be.herokuapp.com";
+    this.sendRequest = function (verb, path, onReadyCallback) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = onReadyCallback;
+        xhr.open(verb, this.serviceEndpointBaseURL + path, true);
+        xhr.send();
+    };
 };
 
 RestAdapter.prototype.all = function (successCallback) {
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function (args) {
+    this.sendRequest("GET", "/courses", function () {
 
         if (this.readyState == this.DONE) {
-            successCallback(null,JSON.parse(this.responseText));
+            successCallback(null, JSON.parse(this.responseText));
         }
-    };
-    xhr.open("GET", this.serviceEndpointBaseURL+"/courses",true);
-    xhr.send();
+    });
+};
+
+RestAdapter.prototype.subscribeParticipant = function (courseCode, callback) {
+    this.sendRequest("POST", "/courses/" + courseCode + "/participants", function () {
+
+        if (this.readyState == this.DONE) {
+            callback(null, "OK");
+        }
+    });
 };
--- app/lib/course_adapter_test.js
+++ app/lib/course_adapter_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe("In-memory Course adapter", function () {
+describe("Course REST adapter", function () {
 
     var onSuccess;
     var adapter;
@@ -15,38 +15,55 @@ describe("In-memory Course adapter", function () {
         jasmine.Ajax.uninstall();
     });
 
-    it('should invoke default /course GET ', function () {
-        adapter.all(onSuccess);
-        expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://antani-be.herokuapp.com/courses');
+    describe('all courses scenario', function () {
+
+        it('should invoke default /courses GET ', function () {
+            adapter.all(onSuccess);
+            expect(jasmine.Ajax.requests.mostRecent().method).toBe('GET');
+            expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://antani-be.herokuapp.com/courses');
+        });
+
+
+        it('should invoke /courses GET ', function () {
+            adapter = new RestAdapter("https://any");
+            adapter.all(onSuccess);
+            expect(jasmine.Ajax.requests.mostRecent().method).toBe('GET');
+            expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://any/courses');
+        });
+
+
+        it('should successfully get not empty course list', function () {
+
+            adapter.all(onSuccess);
+            expect(onSuccess).not.toHaveBeenCalled();
+
+            var mockedRequest = jasmine.Ajax.requests.mostRecent();
+
+            var courseList = '[{ "code": 1234, "title": "gestire gli scope", "date": "12/12/12", "price": 1000},' +
+                '{"code": 2222, "title": "angular ma chissene", "date": "11/11/11", "price": 2000}]';
+            var mockedResponse =
+            {
+                success: {
+                    "status": 200,
+                    "contentType": 'text/plain',
+                    "responseText": courseList
+                }
+            };
+            mockedRequest.respondWith(mockedResponse.success);
+            expect(onSuccess).toHaveBeenCalledWith(null, JSON.parse(courseList));
+        });
+
     });
 
+    describe('new course subscription scenario', function () {
 
-    it('should invoke /course GET ', function () {
-        adapter = new RestAdapter("https://any");
-        adapter.all(onSuccess);
-        expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://any/courses');
+        it('should invoke /courses/[id]/participants POST', function () {
+            adapter.subscribeParticipant('123ABC', onSuccess);
+            expect(jasmine.Ajax.requests.mostRecent().method).toBe('POST');
+            expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://antani-be.herokuapp.com/courses/123ABC/participants');
+        });
+
     });
 
-
-    it('should succesfully get not empty course list', function () {
-
-        adapter.all(onSuccess);
-        expect(onSuccess).not.toHaveBeenCalled();
-
-        var mockedRequest = jasmine.Ajax.requests.mostRecent();
-
-        var courseList ='[{ "code": 1234, "title": "gestire gli scope", "date": "12/12/12", "price": 1000},' +
-            '{"code": 2222, "title": "angular ma chissene", "date": "11/11/11", "price": 2000}]';
-        var mockedResponse =
-        {
-            success: {
-                "status": 200,
-                "contentType": 'text/plain',
-                "responseText": courseList
-            }
-        }
-        mockedRequest.respondWith(mockedResponse.success);
-        expect(onSuccess).toHaveBeenCalledWith(null,JSON.parse(courseList));
-    });
 
 });
